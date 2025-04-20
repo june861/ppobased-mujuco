d9ed1a366d9740f02b54ea2d19cfe51ae1e6e096
THS <ths@bupt.edu.cn>
2025-04-20 13:31:43 +0800

    atari

--- src/scripts/ppo1_atari.sh
+++ src/scripts/ppo1_atari.sh
@@ -1,29 +1,29 @@
-#!binbash
+#!/bin/bash
 
 exp="atari"
 seeds=(1 2 3)
 update_epochs=(4)
-algo="ppo-clip"
+algo="ppo-penalty"
 envs=(
     # VCIS 12
-    "ALE/Assault-v5"
     "ALE/Alien-v5"
+    "ALE/Assault-v5"
+    "ALE/Asteroids-v5"
     "ALE/Amidar-v5"
     "ALE/Asterix-v5"
-    "ALE/Asteroids-v5"
     "ALE/Atlantis-v5"
     "ALE/BankHeist-v5"
+    "ALE/Breakout-v5"
     "ALE/BattleZone-v5"
     "ALE/BeamRider-v5"
     "ALE/Berzerk-v5"
     "ALE/Bowling-v5"
     "ALE/Boxing-v5"
-    "ALE/Breakout-v5"
     "ALE/Centipede-v5"
     "ALE/ChopperCommand-v5"
     "ALE/CrazyClimber-v5"
 
-    # 
+    # VCIS 13
     # "ALE/DemonAttack-v5"
     # "ALE/DoubleDunk-v5"
     # "ALE/Enduro-v5"
@@ -44,6 +44,8 @@ envs=(
     # "ALE/Phoenix-v5"
     # "ALE/Pitfall-v5"
     # "ALE/Pong-v5"
+
+
     # "ALE/PrivateEye-v5"
     # "ALE/Qbert-v5"
     # "ALE/Riverraid-v5"
@@ -61,12 +63,11 @@ envs=(
     # "ALE/WizardOfWor-v5"
     # "ALE/YarsRevenge-v5"
     # "ALE/Zaxxon-v5"
-
 )
 
-# set root dir, it will return "src" abs path
+# set root dir, it will return "src/" abs path
 PROJECT_ROOT=$(dirname "$(dirname "$(realpath "$0")")")
-CONFIG_PATH="$PROJECT_ROOT/conf/dis_exp_run.yaml"
+CONFIG_PATH="$PROJECT_ROOT/conf/dis_ppo1_run.yaml"
 
 # check config file
 if [ ! -f "$CONFIG_PATH" ]; then
@@ -84,8 +85,8 @@ do
             CUDA_VISIBLE_DEVICES=0,1,2,3 python -m src.main --seed $seed  --yaml $CONFIG_PATH --env_id $env_id --env_type $exp --algo $algo
 
             # echo "ppo-clip continous action space"
-            # ppoclip_yaml="homewangchenxuppobased-mujucosrcconfcon_ppoclip_run.yaml"
-            # homewangchenxuanaconda3envsmujoco_-v4binpython .srcalgosmujocorun.py --seed $seed  --yaml $appo_yaml --env_id $env_id --env_type $exp         
+            # ppoclip_yaml="/home/wangchenxu/ppobased-mujuco/src/conf/con_ppoclip_run.yaml"
+            # /home/wangchenxu/anaconda3/envs/mujoco_v4/bin/python ./src/algos/mujoco/run.py --seed $seed  --yaml $appo_yaml --env_id $env_id --env_type $exp         
         done
         echo "Experiment with env_id=$env_id seed=$seed finished."
     done

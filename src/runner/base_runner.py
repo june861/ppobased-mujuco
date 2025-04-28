c950f5e6265d273ad0f4903ad0af99bd7071ec87
june <overtheriver861@gmail.com>
2025-04-28 10:05:53 +0800

    supply

--- src/runner/base_runner.py
+++ src/runner/base_runner.py
@@ -54,7 +54,7 @@ class BaseRunner(ABC):
         if self.all_args.track:
             wandb.init(
                 project=self.all_args.wandb_project_name,
-                group=self.all_args.exp_name + "_pi_old_2",
+                group=self.all_args.exp_name + "_pi_old_3",
                 sync_tensorboard=True,
                 config=vars(self.all_args),
                 name=self.run_name,

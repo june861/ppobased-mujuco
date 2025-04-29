94a9081598f85e6ca1881c4f399890220846ab2d
june <overtheriver861@gmail.com>
2025-04-29 09:35:41 +0800

    add

--- src/algos/ppo_family/appo/con_appo_trainer.py
+++ src/algos/ppo_family/appo/con_appo_trainer.py
@@ -187,19 +187,19 @@ class Continous_APPO_Trainer(BaseTrainer):
         # pg_loss_2 = (mb_returns_vars * 0.5 * mb_old_logprobs[:,1:].exp() * (ratio2 - 1)**2).mean()
         
         
-        # pg_loss1 = -mb_advantages * ratio1
-        # pg_loss2 = -mb_advantages * torch.clamp(ratio1, (1 - self.clip_coef), (1 + self.clip_coef))
-        # pg_loss_1 = torch.max(pg_loss1, pg_loss2).mean()
-        # mb_old_logprobs = kwargs["mb_old_logprobs"]
-        # pg_loss_2 = (0.5 * mb_old_logprobs[:,1:].exp() * (ratio2 - 1)**2).mean()
-        
         pg_loss1 = -mb_advantages * ratio1
         pg_loss2 = -mb_advantages * torch.clamp(ratio1, (1 - self.clip_coef), (1 + self.clip_coef))
         pg_loss_1 = torch.max(pg_loss1, pg_loss2).mean()
         mb_old_logprobs = kwargs["mb_old_logprobs"]
-        new_logprobs = kwargs["new_logprobs"]
-        ratios = (new_logprobs - mb_old_logprobs).exp()
-        pg_loss_2 = (0.5 * mb_old_logprobs.exp() *(ratios - 1)**2).mean()
+        pg_loss_2 = (0.5 * mb_old_logprobs[:,1:].exp() * (ratio2 - 1)**2).mean()
+        
+        # pg_loss1 = -mb_advantages * ratio1
+        # pg_loss2 = -mb_advantages * torch.clamp(ratio1, (1 - self.clip_coef), (1 + self.clip_coef))
+        # pg_loss_1 = torch.max(pg_loss1, pg_loss2).mean()
+        # mb_old_logprobs = kwargs["mb_old_logprobs"]
+        # new_logprobs = kwargs["new_logprobs"]
+        # ratios = (new_logprobs - mb_old_logprobs).exp()
+        # pg_loss_2 = (0.5 * mb_old_logprobs.exp() *(ratios - 1)**2).mean()
 
         pg_loss = pg_loss_1  +  pg_loss_2
         
--- src/runner/base_runner.py
+++ src/runner/base_runner.py
@@ -50,11 +50,11 @@ class BaseRunner(ABC):
     def setup_logging(self):
         """Setup wandb and TensorBoard logging."""
         self.run_name = f"{self.all_args.exp_name}_seed{self.all_args.seed}_{int(time.time())}_{os.getppid()}"
-        self.all_args.logger.info(f"wandb project is {self.all_args.wandb_project_name}, run name is {self.run_name}, group name is {self.all_args.exp_name  + '_pi_old_1'}!")
+        self.all_args.logger.info(f"wandb project is {self.all_args.wandb_project_name}, run name is {self.run_name}, group name is {self.all_args.exp_name  + '_pi_old'}!")
         if self.all_args.track:
             wandb.init(
                 project=self.all_args.wandb_project_name,
-                group=self.all_args.exp_name + "_pi_old_3",
+                group=self.all_args.exp_name + "_pi_old",
                 sync_tensorboard=True,
                 config=vars(self.all_args),
                 name=self.run_name,
